073760875ebb0281a4cd8dbd8a43f5f3f8bc7de6
toptops <toptops@toptopsui-MacBookPro.local>
2023-10-24 15:43:39 +0900

    [ADD] layered-architecture 추가

---
+++ architecture/layered-architecture/src/main/java/com/layered/LayeredArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.layered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeredArchitectureApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LayeredArchitectureApplication.class, args);
+  }
+
+}
---
+++ architecture/layered-architecture/src/test/java/com/layered/LayeredArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.layered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayeredArchitectureApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
