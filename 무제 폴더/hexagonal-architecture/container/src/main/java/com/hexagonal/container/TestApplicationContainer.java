0d142aea93bf7503f0a44d6db2935537cf3ad3ec
toptops <toptops@toptopsui-MacBookPro.local>
2023-10-24 15:33:00 +0900

    init

---
+++ 무제 폴더/hexagonal-architecture/container/src/main/java/com/hexagonal/container/TestApplicationContainer.java
@@ -0,0 +1,12 @@
+package com.hexagonal.container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplicationContainer {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestApplicationContainer.class, args);
+  }
+}
